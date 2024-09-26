0153590dd6afe1b5d2e9124e8f9beb41fce6de6d
KevAkaSlayer <aminalislam4223@gmail.com>
2024-09-26 23:44:42 +0600

    done everything

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function getAmount(id){
     const amount = parseInt(document.getElementById(id).value);
+    
     if(isNaN(amount) || amount < 0){
         alert('Please enter a valid amount');
         document.getElementById('inputN').value = '';
@@ -25,58 +26,61 @@ function orgName(id){
 
 const select = document.getElementById('history');
 
-// const inputNoa = document.getElementById('inputN');
-// const inputFeni = document.getElementById('inputF');
-// const inputPro = document.getElementById('inputP');
-// inputNoa.addEventListener('input', function(e){
-//     const inputVal = e.target.value;
-
-//     if(inputVal.length >= 1){
-//         document.getElementById('NBtn').removeAttribute('disabled');
-//     }
-// });
-
-// inputFeni.addEventListener('input', function(e){
-//     const inputVal = e.target.value;
-
-//     if(inputVal.length >= 1){
-//         document.getElementById('FBtn').removeAttribute('disabled');
-//     }
-// });
-// inputPro.addEventListener('input', function(e){
-//     const inputVal = e.target.value;
-
-//     if(inputVal.length >= 1){
-//         document.getElementById('PBtn').removeAttribute('disabled');
-//     }
-// });
 
 
+document.getElementById("NBtn").disabled = true;
 
-
-
-document.getElementById('NBtn').addEventListener('click', function(){
-   if(totalAmount() > 0 && totalAmount() >= getAmount('inputN')){
-    
-        const noaAmount = getAmount('inputN');
-        const noaTotal = showAmount('nCash');
-        const noaTotalAmount = noaAmount + noaTotal;
-        total = totalAmount() -  noaAmount;
-        document.getElementById('nCash').innerText = noaTotalAmount;
-        document.getElementById('credit').innerText = total;
-        document.getElementById('inputN').value = '';
-        const name = orgName('noakhali');
-        select.innerHTML += `<div class="border border-green-100 rounded-lg flex flex-col p-5 gap-3">
-                        <h3 class="text-xl font-bold md:text-3xl">${noaAmount} Taka is Donated in "${name}"</h3>
-                        <p class="text-md">Date : ${Date()}</p>
-                    </div>`
-        document.getElementById('my_modal_1').classList.remove('hidden');
-   }
-   else{
-         alert('Please add money to your account');
-   }
-   
+document.getElementById("inputN").addEventListener("input", function (e) {
+  const inputVal = e.target.value;
+  if (inputVal.length >= 1 && !isNaN(inputVal) && parseInt(inputVal) > 0) {
+    document.getElementById("NBtn").disabled = false;
+  } else {
+    document.getElementById("NBtn").disabled = true; 
+  }
 });
+document.getElementById("FBtn").disabled = true;
+
+document.getElementById("inputF").addEventListener("input", function (e) {
+  const inputVal = e.target.value;
+  if (inputVal.length >= 1 && !isNaN(inputVal) && parseInt(inputVal) > 0) {
+    document.getElementById("FBtn").disabled = false;
+  } else {
+    document.getElementById("FBtn").disabled = true; 
+  }
+});
+
+document.getElementById("PBtn").disabled = true;
+
+document.getElementById("inputP").addEventListener("input", function (e) {
+  const inputVal = e.target.value;
+  if (inputVal.length >= 1 && !isNaN(inputVal) && parseInt(inputVal) > 0) {
+    document.getElementById("PBtn").disabled = false;
+  } else {
+    document.getElementById("PBtn").disabled = true; 
+  }
+});
+
+document.getElementById("NBtn").addEventListener("click", function () {
+  if (totalAmount() > 0 && totalAmount() >= getAmount("inputN")) {
+    const noaAmount = getAmount("inputN");
+    const noaTotal = showAmount("nCash");
+    const noaTotalAmount = noaAmount + noaTotal;
+    total = totalAmount() - noaAmount;
+    document.getElementById("nCash").innerText = noaTotalAmount;
+    document.getElementById("credit").innerText = total;
+    document.getElementById("inputN").value = "";
+    const name = orgName("noakhali");
+    select.innerHTML += `<div class="border border-green-100 rounded-lg flex flex-col p-5 gap-3">
+                                <h3 class="text-xl font-bold md:text-3xl">${noaAmount} Taka is Donated in "${name}"</h3>
+                                <p class="text-md">Date : ${Date()}</p>
+                             </div>`;
+    document.getElementById("my_modal_1").classList.remove("hidden");
+    document.getElementById("NBtn").disabled = true; 
+  } else {
+    alert("Please add money to your account");
+  }
+});
+
 
 
 document.getElementById('FBtn').addEventListener('click', function(){
@@ -92,9 +96,9 @@ document.getElementById('FBtn').addEventListener('click', function(){
          select.innerHTML += `<div class="border border-green-100 rounded-lg flex flex-col p-5 gap-3">
                          <h3 class="font-bold text-3xl">${feniAmount} Taka is Donated in "${name}"</h3>
                          <p class="text-md">Date : ${Date()}</p>
-                     </div>`
-         
-
+                     </div>`;
+        document.getElementById("my_modal_2").classList.remove("hidden");
+        document.getElementById("FBtn").disabled = true;        
 
     }
     else{
@@ -117,7 +121,9 @@ document.getElementById('FBtn').addEventListener('click', function(){
          select.innerHTML += `<div class="border border-grey-100 rounded-lg flex flex-col p-5 gap-3">
                          <h3 class="font-bold text-3xl">${proAmount} Taka is Donated in "${name}"</h3>
                          <p class="text-md">Date : ${Date()}</p>
-                     </div>`
+                     </div>`;
+        document.getElementById("my_modal_3").classList.remove("hidden");
+        document.getElementById("PBtn").disabled = true; 
     }
     else{
           alert('Please add money to your account');
